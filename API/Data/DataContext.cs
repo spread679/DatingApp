f6b72920cf872e8defc13ee7cfb98b60080dc9fd
Spread <riccalira95@protonmail.com>
2022-07-04 21:55:33 +0200

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext
+        : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
